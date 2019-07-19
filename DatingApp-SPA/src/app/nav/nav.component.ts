4a4e354952b5d52f82aa0375faa89d0616c1b23e
konolga <olgakon.mail@gmail.com>
2019-07-20 00:22:48 +0300

    login frontend

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AuthService } from '../_services/auth.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('logged in successfully');
+    },
+    error => {
+      console.log('Falled to login');
+    });
   }
 
 }
