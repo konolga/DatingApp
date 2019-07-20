719eb894f7cfe2b723c261621a2a0dd2097a48f5
konolga <olgakon.mail@gmail.com>
2019-07-20 19:50:02 +0300

    Exception handeling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -55,6 +55,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
          public async Task<IActionResult> Login (UserForLoginDto userForLoginDto)
          {
+
+            throw new Exception ("ex");
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
             if(userFromRepo == null)
                 return Unauthorized();
