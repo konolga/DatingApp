9e988b68da3d1c3d62892c2fe10b47bba3cb40e4
konolga <olgakon.mail@gmail.com>
2019-07-20 15:18:18 +0300

    Registration form and buttons

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,16 +4,18 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -22,7 +24,7 @@ import { AuthService } from './_services/auth.service';
       FormsModule
    ],
    providers: [
-     AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
