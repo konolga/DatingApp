5deb824a6da977c7cdab9fb373dc5ca1ca5c9c7a
konolga <olgakon.mail@gmail.com>
2019-08-07 00:15:21 +0300

    jwt helper to check token

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -28,8 +28,9 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    //const token = localStorage.getItem('token');
+    //return !!token;
+    return this.authService.loggedIn();
   }
   logout() {
     localStorage.removeItem('token');
